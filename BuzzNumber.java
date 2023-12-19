b5e231bfa506483a6fbc39cab44eb5c754c26564
Sourabshukla <109239285+Sourabshukla@users.noreply.github.com>
2023-12-19 07:09:46 +0530

    Update BuzzNumber.java

--- BuzzNumber.java
+++ BuzzNumber.java
@@ -1,5 +1,7 @@
-package NumericalPattern;
+//those numbers that are divisible by seven or end with seven. For example, 57 is a buzz number because the number ends with seven.
+//Another example is 28 because the number is divisible by seven.
 
+package NumericalPattern;
 import java.util.Scanner;
 public class BuzzNumber {
 	public static void main(String[] args) {
